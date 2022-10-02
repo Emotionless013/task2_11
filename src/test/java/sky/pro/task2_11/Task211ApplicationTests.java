93eeddecce06855b99b64f88d3b48ad39701773e
Alexander <emotionless013@mail.ru>
2022-10-02 11:47:24 +0400

    Initial commit

---
+++ src/test/java/sky/pro/task2_11/Task211ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.task2_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
